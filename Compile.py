ec0ae39c9bdca9bf6dc2674b0ced71f6109aeeed
AndZRi <68921308+AndZRi@users.noreply.github.com>
2025-05-17 20:51:40 +0300

    Made a compiler script with auto including all the necessary files from Resources.py

---
+++ Compile.py
@@ -0,0 +1,11 @@
+from Resources import RelativePaths
+
+import PyInstaller.__main__
+
+PyInstaller.__main__.run([
+    'main.py',
+    '--name=Anzigos Gesture Drawing',
+    '--onefile',
+    '--noconsole',
+    *[f'--add-data={getattr(RelativePaths, i)}:./{getattr(RelativePaths, i)}' for i in dir(RelativePaths) if not i.startswith('__')]
+])
