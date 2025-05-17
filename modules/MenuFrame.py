72dfcf64d73f3dcdd724e1490f683869f2c33a8e
AndZRi <68921308+AndZRi@users.noreply.github.com>
2025-05-17 21:19:26 +0300

    a bit commenting and renaming

--- modules/MenuFrame.py
+++ modules/MenuFrame.py
@@ -59,32 +59,37 @@ class MenuFrame(ttk.Frame):
         self.grid_rowconfigure(3, weight=1)
         self.grid(column=0, row=0, sticky=NSEW)
 
+        # folder frame
         folder_frame = ttk.Frame(self, padding='12 12 12 12')
         folder_frame.grid(column=1, row=1, sticky=NSEW)
-        folder_button = ttk.Button(folder_frame, text="Pick folder", command=self.pick_folder)
-        folder_button.grid(column=1, row=1, sticky=EW)
         folder_frame.grid_columnconfigure(2, weight=1)
-        folder_entry = ttk.Entry(folder_frame, textvariable=self.source_dir)
-        folder_entry.grid(column=2, row=1, sticky=EW)
 
+        folder_button   = ttk.Button(folder_frame, text="Pick folder", command=self.pick_folder)
+        folder_entry    = ttk.Entry(folder_frame, textvariable=self.source_dir)
+
+        folder_button.  grid(column=1, row=1, sticky=EW)
+        folder_entry.   grid(column=2, row=1, sticky=EW)
+
+        # time picking frame
         time_frame = ttk.Frame(self, padding='12 12 12 12')
         time_frame.grid(column=1, row=2, sticky=NSEW)
-
-        time_label = ttk.Label(time_frame, text="Time: ", font=20)
-        time_label.grid(column=1, row=1, sticky=NSEW)
         time_frame.grid_columnconfigure(1, weight=1)
         time_frame.grid_rowconfigure(1, weight=1)
 
-        minutes_entry = ttk.Entry(time_frame, textvariable=self.mins, width=10)
-        minutes_entry.grid(column=2, row=1)
+        time_label      = ttk.Label(time_frame, text="Time: ", font=20)
+        minutes_entry   = ttk.Entry(time_frame, textvariable=self.mins, width=10)
+        secs_entry      = ttk.Entry(time_frame, textvariable=self.secs, width=10)
         ttk.Label(time_frame, text='m   ', font=20).grid(column=3, row=1, sticky=W)
-
-        secs_entry = ttk.Entry(time_frame, textvariable=self.secs, width=10)
-        secs_entry.grid(column=4, row=1)
         ttk.Label(time_frame, text='s', font=20).grid(column=5, row=1, sticky=W)
 
+        time_label.     grid(column=1, row=1, sticky=NSEW)
+        minutes_entry.  grid(column=2, row=1)
+        secs_entry.     grid(column=4, row=1)
+
+        # start button
         start_button = ttk.Button(self, text="Start", command=self.finish)
         start_button.grid(column=1, row=3, sticky=NSEW)
+
 
         for child in self.winfo_children():
             child.grid_configure(padx=5, pady=5)
