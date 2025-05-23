e1b0156ecdffcaa0a9f94524d1e12af41e2d3d76
AndZRi <68921308+AndZRi@users.noreply.github.com>
2025-05-23 16:49:18 +0300

    update compile settings
    - now creates both --onefile and folder versions

--- Compile.py
+++ Compile.py
@@ -2,10 +2,12 @@ from Resources import RelativePaths
 
 import PyInstaller.__main__
 
-PyInstaller.__main__.run([
+args = [
     'main.py',
     '--name=Anzigos Gesture Drawing',
-    '--onefile',
     '--noconsole',
     *[f'--add-data={getattr(RelativePaths, i)}:./{getattr(RelativePaths, i)}' for i in dir(RelativePaths) if not i.startswith('__')]
-])
+]
+
+PyInstaller.__main__.run(args)
+PyInstaller.__main__.run(args + ['--onefile'])
