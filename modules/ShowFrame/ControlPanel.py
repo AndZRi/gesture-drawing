d6ab065bd3175cab7eedeb5dbe831d53e3568dae
AndZRi <68921308+AndZRi@users.noreply.github.com>
2025-05-24 17:01:34 +0300

    implemented ControlPanel

--- modules/ShowFrame/ControlPanel.py
+++ modules/ShowFrame/ControlPanel.py
@@ -2,30 +2,41 @@ from tkinter import ttk
 from tkinter import *
 from collections.abc import Callable
 
+from PIL import ImageTk
 from Resources import Processed
 
-class ControlPanel(Frame):
+class ControlPanel(ttk.Frame):
     def __init__(self, master):
         super().__init__(master)
         self.paused = False
-        self.configure(background='black', height=50)
         self.previous_button_listeners: list[Callable] = []
         self.play_button_listeners: list[Callable] = [self.switch_pause]
         self.next_button_listeners: list[Callable] = []
 
+        self.init_widgets()
+
     def init_widgets(self):
-        play_button = Button(self, image=Processed.PlayIcon, command=self.on_play_button_clicked)
-        previous_button = Button(self, image=Processed.PreviousIcon, command=self.on_previous_button_clicked)
-        next_button = Button(self, image=Processed.NextIcon, command=self.on_next_button_clicked)
+        self.grid_rowconfigure(0, weight=1)
+        self.grid_columnconfigure(0, weight=1)
+        self.grid_columnconfigure(1, weight=0)
+        self.grid_columnconfigure(2, weight=1)
+        self.grid_propagate(True)
 
-        # previous_button.grid(column=0, row=0)
-        # play_button.grid(column=1, row=0)
-        # next_button.grid(column=2, row=0)
+        play_img = ImageTk.PhotoImage(Processed.PlayIcon)
+        play_button = ttk.Button(self, image=play_img, command=self.on_play_button_clicked)
+        play_button.image = play_img
 
-        previous_button.pack(side=LEFT)
-        play_button.pack(side=BOTTOM)
-        next_button.pack(side=RIGHT)
+        prev_img = ImageTk.PhotoImage(Processed.PreviousIcon)
+        previous_button = ttk.Button(self, image=prev_img, command=self.on_previous_button_clicked)
+        previous_button.image = prev_img
 
+        next_img = ImageTk.PhotoImage(Processed.NextIcon)
+        next_button = ttk.Button(self, image=next_img, command=self.on_next_button_clicked)
+        next_button.image = next_img
+
+        previous_button.grid(column=0, row=0, sticky=E)
+        play_button.grid(column=1, row=0)
+        next_button.grid(column=2, row=0, sticky=W)
 
     def switch_pause(self):
         self.paused = not self.paused
